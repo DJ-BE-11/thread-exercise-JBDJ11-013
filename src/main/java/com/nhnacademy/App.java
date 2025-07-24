88f12b2f45bb5d3c3b8bb03ce8bd778d50c5ae93
chaewonn <scw0629@naver.com>
2025-07-24 19:44:23 +0900

    exercise-step1-9 구현 완료 V01

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -22,35 +22,44 @@ import javax.management.monitor.Monitor;
 public class App
 {
 
-    //TODO#1 monitor로 사용한 객체를 생성 합니다.
-    public static Object monitor;
+    // monitor로 사용할 객체를 생성 합니다.
+    public static Object monitor = new Object();
 
     public static void main( String[] args )
     {
 
-        //TODO#2 counterHandlerA 객체를 생성 합니다. countMaxSize : 10, monitor
-        CounterHandler counterHandlerA = null;
+        // counterHandlerA 객체를 생성 합니다. countMaxSize : 10, monitor
+        CounterHandler counterHandlerA = new CounterHandler(10, monitor);
 
-        //threadA 생성시 counterHandlerA 객체를 paramter로 전달 합니다.
+        // threadA 생성시 counterHandlerA 객체를 paramter로 전달 합니다.
         Thread threadA = new Thread(counterHandlerA);
 
-        //threadA의 name을 'my-counter-A' 로 설정 합니다.
+        // threadA의 name을 'my-counter-A' 로 설정 합니다.
         threadA.setName("my-counter-A");
         log.debug("threadA-state:{}",threadA.getState());
 
-        //threadA를 시작 합니다.
+        // threadA를 시작 합니다.
         threadA.start();
         log.debug("threadA-state:{}",threadA.getState());
 
-        //TODO#3 - Main Thread에서 2초 후 monitor를 이용하여 대기하고 있는 threadA를 깨움 니다.
+        // Main Thread에서 2초 후 monitor를 이용하여 대기하고 있는 threadA를 깨웁 니다.
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
+        synchronized (monitor) {
+            log.debug("call monitor.notify()");
+            monitor.notify();
+        }
 
-        //Main Thread가 threadA  종료될 때 까지 대기 합니다. Thread.yield를 사용 합니다.
+        // Main Thread가 threadA  종료될 때 까지 대기 합니다. Thread.yield를 사용 합니다.
         do {
             Thread.yield();
         }while (threadA.isAlive());
 
-        //'Application exit!' message를 출력 합니다.
+        // 'Application exit!' message를 출력 합니다.
         log.debug("Application exit!");
 
     }
--- src/main/java/com/nhnacademy/thread/CounterHandler.java
+++ src/main/java/com/nhnacademy/thread/CounterHandler.java
@@ -14,6 +14,8 @@ package com.nhnacademy.thread;
 
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Objects;
+
 @Slf4j
 public class CounterHandler implements Runnable  {
     private final Object monitor;
@@ -22,18 +24,29 @@ public class CounterHandler implements Runnable  {
     private long count;
 
     public CounterHandler(long countMaxSize, Object monitor) {
-        //TODO#4 countMaxSize<=0 or monitor 객체가 null 이면 IllegalArgumentException이 발생 합니다.
+        // countMaxSize<=0 or monitor 객체가 null 이면 IllegalArgumentException이 발생 합니다.
+        if ((countMaxSize <= 0) || Objects.isNull(monitor)) {
+            throw new IllegalArgumentException("countMaxSize는 0보다 커야 하며, monitor 객체는 null일 수 없습니다.");
+        }
 
+        // countMaxSize, count, monitor 변수를 초기화 합니다.
+        this.countMaxSize = countMaxSize;
+        this.monitor = monitor;
 
-        //TODO#5  countMaxSize, count, monitor 변수를 초기화 합니다.
-
+        this.count = 0;
     }
 
     @Override
     public void run() {
-        //TODO#6 Thread에 의해서 run() method가 호출되면 무한 대기 합니다. monitor객체를 이용해서 구현하세요
-        //monitor는 여러 Thread가 동시에 접근할 수 없도록  접근을 제어해야 합니다.
-
+        // Thread에 의해서 run() method가 호출되면 무한 대기 합니다. monitor객체를 이용해서 구현하세요
+        // monitor는 여러 Thread가 동시에 접근할 수 없도록  접근을 제어해야 합니다.
+        synchronized (monitor) {
+            try {
+                monitor.wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
 
         do {
             try {
@@ -44,6 +57,6 @@ public class CounterHandler implements Runnable  {
             count++;
             log.debug("thread:{},state:{},count:{}",Thread.currentThread().getName(),Thread.currentThread().getState(),count);
 
-        }while (count<countMaxSize);
+        }while (count < countMaxSize);
     }
 }
