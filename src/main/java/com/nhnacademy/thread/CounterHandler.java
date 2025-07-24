4b2a894689609ffa42347abd04a1c7a647984e11
chaewonn <scw0629@naver.com>
2025-07-24 16:04:38 +0900

    exercise-step1-6-counter-join-2 구현 완료 V01

--- src/main/java/com/nhnacademy/thread/CounterHandler.java
+++ src/main/java/com/nhnacademy/thread/CounterHandler.java
@@ -21,7 +21,7 @@ public class CounterHandler implements Runnable  {
     private long count;
 
     public CounterHandler(long countMaxSize) {
-        if(countMaxSize<=0){
+        if(countMaxSize <= 0){
             throw new IllegalArgumentException();
         }
 
@@ -39,6 +39,6 @@ public class CounterHandler implements Runnable  {
             }
             count++;
             log.debug("thread:{},state:{},count:{}",Thread.currentThread().getName(),Thread.currentThread().getState(),count);
-        }while (count<countMaxSize);
+        }while (count < countMaxSize);
     }
 }
