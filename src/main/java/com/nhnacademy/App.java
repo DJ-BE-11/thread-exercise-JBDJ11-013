0706ac045999826830a0d045ea8702d489f2492a
chaewonn <scw0629@naver.com>
2025-07-25 16:29:52 +0900

    step3-3 동작 확인 및 주석 설명 작성 완료 V01

--- src/main/java/com/nhnacademy/App.java
+++ src/main/java/com/nhnacademy/App.java
@@ -25,34 +25,34 @@ public class App
     public static void main( String[] args )
     {
 
-        //TODO#1 shardCounter 객체를 0으로 초기화 합니다.
+        // shardCounter 객체를 0으로 초기화 합니다.
         SharedCounter sharedCounter = new SharedCounter(0l);
 
-        //TODO#2 counterIncreaseHandler 객체를 생성 합니다.
+        // counterIncreaseHandler 객체를 생성 합니다.
         CounterIncreaseHandler counterIncreaseHandler = new CounterIncreaseHandler(sharedCounter);
 
-        //TODO#3 counterIncreaseHandler를 이용해서 threadA ~ E 를 생성 합니다.
+        // counterIncreaseHandler를 이용해서 threadA ~ E 를 생성 합니다.
         Thread threadA = new Thread(counterIncreaseHandler, "thread-A");
         Thread threadB = new Thread(counterIncreaseHandler, "thread-B");
         Thread threadC = new Thread(counterIncreaseHandler, "thread-C");
         Thread threadD = new Thread(counterIncreaseHandler, "thread-D");
         Thread threadE = new Thread(counterIncreaseHandler, "thread-E");
 
-        //TODO#4 thrad의 우선순위를 지정할 수 있지만, 실질적인 관리는 운영체제에서 함으로, 동작하는 순서는 다를 수 있습니다.
+        // thread의 우선순위를 지정할 수 있지만, 실질적인 관리는 운영체제에서 함으로, 동작하는 순서는 다를 수 있습니다.
         threadA.setPriority(10);
         threadB.setPriority(1);
         threadC.setPriority(1);
         threadD.setPriority(1);
         threadE.setPriority(1);
 
-        //TODO#5 threadA ~ E 를 시작 합니다.
+        // threadA ~ E 를 시작 합니다.
         threadA.start();
         threadB.start();
         threadC.start();
         threadD.start();
         threadE.start();
 
-        //TODO#6 main thread가 실행 후 10초 후 threadA, threadB 종료될 수 있도록 interrupt 발생 시킵니다.
+        // main thread가 실행 후 10초 후 threadA ~ E 종료될 수 있도록 interrupt 발생 시킵니다.
         try {
             Thread.sleep(10000);
             threadA.interrupt();
@@ -64,7 +64,7 @@ public class App
             throw new RuntimeException(e);
         }
 
-        //TODO#7 main Thread는 threadA와 threadB의 상태가 terminated가 될 때 까지 대기 합니다. 즉 threadA, threadB가 종료될 때 까지 대기(양보) 합니다.
+        // main Thread는 threadA ~ E의 상태가 terminated가 될 때 까지 대기 합니다. 즉 threadA ~ E가 종료될 때 까지 대기(양보) 합니다.
         while (threadA.isAlive() && threadB.isAlive() && threadC.isAlive() && threadD.isAlive() && threadE.isAlive() ){
             Thread.yield();
         }
--- src/main/java/com/nhnacademy/thread/CounterIncreaseHandler.java
+++ src/main/java/com/nhnacademy/thread/CounterIncreaseHandler.java
@@ -24,36 +24,38 @@ public class CounterIncreaseHandler implements Runnable {
     private final List<String> threadHistory;
 
     public CounterIncreaseHandler(SharedCounter sharedCounter) {
-        //sharedCounter를 초기화 합니다.  sharedCounter가 null 이면 IllegalArgumentException이 발생 합니다.
+        // sharedCounter를 초기화 합니다.  sharedCounter가 null 이면 IllegalArgumentException이 발생 합니다.
         if(Objects.isNull(sharedCounter)){
             throw new IllegalArgumentException(String.format("SharedCount is null"));
         }
+
         this.sharedCounter = sharedCounter;
         this.threadHistory = Collections.synchronizedList(new ArrayList<>());
     }
 
     @Override
     public void run() {
-        //TODO#8 현제 Thread의 interrupted이 ture <--  while의 종료조건 : interrupt가 발생 했다면 종료 합니다.
+        // 현재 Thread의 interrupted이 true <--  while의 종료조건 : interrupt가 발생 했다면 종료 합니다.
         while(!Thread.currentThread().isInterrupted()) {
             try {
                 Thread.sleep(1000);
-                //TODO#9 sharedCounter의 count를 1증가 시키고 count값을 반환 합니다.
+                // sharedCounter의 count를 1증가 시키고 count값을 반환 합니다.
                 long count = sharedCounter.increaseAndGet();
 
-                // A~ E Thread가 공유자원을 사용한 횟수 를 맵에 등록 합니다.
+                // A~ E Thread가 공유자원을 사용한 횟수를 맵에 등록 합니다.
                 threadHistory.add(Thread.currentThread().getName());
                 log.debug("thread:{}, count:{}", Thread.currentThread().getName(), count);
 
-                //TODO#10 - A ~ E thread 중 먼저 실행하는 Thread가 무한 점유 합니다. 즉 다른 Thread는 increaseAndGet()를 실행 할 수 없습니다.
-                synchronized (this) {
-                    while (true) {
+                // A ~ E thread 중 먼저 실행하는 Thread가 무한 점유 합니다. 즉 다른 Thread는 increaseAndGet()를 실행 할 수 없습니다.
+                synchronized (this) {   // 무한 점유: Starvation 현상 발생의 핵심
+                    while (true) {      // 무한 루프
                         log.debug("{} is working",Thread.currentThread().getName());
                     }
                 }
             } catch (Exception e) {
                 log.debug("{} - interrupt!",Thread.currentThread().getName());
-                //현제 Thread에 interrupt()를 호출하여 interrput()를 발생 시킵 니다. 즉 현제 Thread의 interrupted 값이 -> true로 변경 됩니다. -> 즉 while 문을 종료하게 됩니다.
+
+                // 현재 Thread에 interrupt()를 호출하여 interrupt()를 발생 시킵 니다. 즉 현재 Thread의 interrupted 값이 -> true로 변경 됩니다. -> 즉 while 문을 종료하게 됩니다.
                 Thread.currentThread().interrupt();
             }
         }
